8391b242988c9d3d279ac623a7b77ed052586b8e
Fatihbahar <sakinoyuncular@outlook.com>
2023-12-06 16:15:37 +0300

    requrequirements added

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "MLOPS"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "fatih-crypto"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "sakinoyuncular@outlook.com"
 
 
 setuptools.setup(
